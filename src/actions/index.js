483e2a65bc4304eeaf227f25060ff037e654dc85
YuriNem <PsychoactivePie@gmail.com>
2018-09-01 14:00:50 +0300

    Added Redux

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const showListLayout = createAction('SHOW_LIST_LAYOUT');
-
-export const showListFrontend = createAction('SHOW_LIST_FRONTEND');
-
-export const showListCommon = createAction('SHOW_LIST_COMMON');
